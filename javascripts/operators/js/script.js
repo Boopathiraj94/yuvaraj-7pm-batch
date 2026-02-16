1ed7f3fea3a8d99a3a59f17ba454858b1a067d7a
Boopathiraj94 <boopathirajk55@gmail.com>
2026-02-16 20:49:18 +0530

    airthmatic operator

---
+++ javascripts/operators/js/script.js
@@ -0,0 +1,90 @@
+
+/*
+ + , - , * , / , % , ++ , -- , **
+ 
+
+ Not a Number
+
+ 2 power 3
+
+ 2x2x2
+
+
+1) 5 subject marks => ta, en,mat,sci,ss
+ total , avg
+
+
+ 2)
+
+ let a = 5
+
+    a = a++ 
+    --a
+    ++a
+    a++
+
+    log(a)
+3)
+  const b = 5
+    b++
+    --b
+    log(b)
+
+4)
+  let c = 5
+  let d = 2
+   c = c**d
+   d = ++c
+   log(c, d)
+
+
+
+*/
+
+// let a = 3
+
+// let ans = a**3
+
+// console.log(ans);
+
+
+
+//   let a = Number(prompt("enter the a value?"))
+//   let b = Number(prompt("Enter the b value?"))
+
+//   console.log("Addition is =>",a+b);
+//   console.log("substraction is =>",a-b);
+//   console.log("multiplication is =>",a*b);
+//   console.log("division is =>",a/b);
+//   console.log("modulo is =>",a%b);
+
+
+
+// ++ , --
+
+// post incre  a++ / pre incre   ++a   => a = a + 1
+//  post dec a--/ pre dec --a   => a = a - 1
+
+// 
+
+
+  let a  = 5
+
+//   console.log(a++);
+//   console.log(a++);
+//   console.log(a);
+//   console.log(a--);
+//   console.log(a--);
+//   console.log(a);
+  
+  
+  
+
+//   console.log(++a);
+//   console.log(++a);
+//   console.log(--a);
+//   console.log(--a);
+//   console.log(--a);
+  
+
+  
